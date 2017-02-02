1853039be46f5130051c5a3d4c20226cbadb5147
Sebastien Diaz <sebastien.diaz@misys.com>
2017-02-02 14:22:17 +0100

    More

--- histo.R
+++ histo.R
@@ -1,3 +1,6 @@
+library(neuralnet)
+library(rpart)
+
 mydata = read.csv("MyData.csv")
 boxplot(mydata$col14)
 hist(mydata$col14)
@@ -7,6 +10,28 @@ mydata$quartile <- with(mydata, cut(mydata$col14,breaks=quantile(mydata$col14, p
 table(mydata$quartile)
 mydata$quartileBin=as.numeric(mydata$quartile)
 mydata<-mydata[complete.cases(mydata), ]
-library(neuralnet)
+
+
+## 75% of the sample size
+smp_size <- floor(0.75 * nrow(mydata))
+
+## set the seed to make your partition reproductible
+set.seed(123)
+mydata_ind <- sample(seq_len(nrow(mydata)), size = smp_size)
+
+train <- mydata[mydata_ind, ]
+test <- mydata[-mydata_ind, ]
+train_ <- train
+test_ <- test
+
 f <- as.formula("quartileBin ~ col2 + col3 + col4 + col5 + col6 + col7 + col8 + col9 + col10 + col11")
-net<-neuralnet(f,data=mydata[,c(3:12,17)],hidden=c(1000,1000,1000),linear.output=F)
+net<-neuralnet(f,data=train[,c(3:12,17)],hidden=c(100,100,100,50),linear.output=F)
+print(net)
+test.res=compute(net,test[,c(3:12)])$net.result
+test<- test[order(test.res),]
+table(as.integer(test.res),test$quartileBin)
+
+fit<-rpart(f, data=train[,c(3:12,17)], method="class")
+printcp(fit) # display the results
+plotcp(fit) # visualize cross-validation results
+summary(fit)
