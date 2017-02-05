e672458f7d5d04435e479133d2316b84ca6707ca
Sebastien DIAZ <sebastien.diaz@misys.com>
2017-02-05 15:31:23 +0100

    Ref

--- createOne.py
+++ createOne.py
@@ -3,6 +3,17 @@ from datetime import datetime
 from operator import itemgetter, attrgetter, methodcaller
 import sklearn.preprocessing as preprocessing
 import numpy as np
+import keras
+from keras.utils import np_utils
+# from keras.layers.core import Dense, Activation, Dropout
+#https://www.kaggle.com/liwste/digit-recognizer/simple-deep-mlp-with-keras/run/2666
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+
+import pandas as pd
+import numpy as np
 class Action:
         def __init__(self, idref, date, ouverture,haut,bas,fermeture,nbTitre):
                 self.idref = idref
@@ -38,20 +49,35 @@ with open('complete.csv', 'rb') as csvfile:
          
 print "sort alogo"
 savedSorted=sorted(student_tuples, key=getKey)
+input_dim = 30
+nb_classes = 3
+model = Sequential()
+# Dense(64) is a fully-connected layer with 64 hidden units.
+# in the first layer, you must specify the expected input data shape:
+# here, 20-dimensional vectors.
+model.add(Dense(500, input_dim=input_dim, init='uniform'))
+model.add(Activation('tanh'))
+model.add(Dropout(0.6))
+model.add(Dense(250, init='uniform'))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
+model.add(Dense(50, init='uniform'))
+model.add(Activation('tanh'))
+model.add(Dropout(0.5))
 
-with open('completeSorted.csv', 'w') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=';',
-                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for row in savedSorted:
-            spamwriter.writerow([row.idref,row.date,row.mktime,row.ouverture,row.haut,row.bas,row.fermeture,row.nbTitre])
-
-
+model.add(Dense(nb_classes, init='uniform'))
+model.add(Activation('softmax'))
+model.load_weights("weights.h5py")
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
 refidref=""
 listValues=[]
 nbValue=30
 decay=1
 ecart=2
-with open('alignedSorted.csv', 'w') as csvfile:
+with open('selectedScoredSorted.csv', 'w') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=';',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
     head=["X{:02d}".format(x) for x in range(nbValue)]
@@ -60,17 +86,12 @@ with open('alignedSorted.csv', 'w') as csvfile:
         if refidref=="":
                 refidref=row.idref
         if refidref!=row.idref:
-                for i in range(len(listValues)/nbValue):
-                        if i+nbValue+decay+ecart<len(listValues):
-                                norm =np.asarray(listValues[i:i+nbValue])
-                                taux=listValues[i+nbValue+decay+ecart]-listValues[i+nbValue+decay]
-                                score=0
-                                
-                                if taux > 0.05 :
-                                        score =1
-                                if taux > 0.2 :
-                                        score =2                                        
-                                spamwriter.writerow([refidref]+preprocessing.scale(norm).tolist() +[taux,score])                        
+                if len(listValues)-nbValue>=0:
+                        listValues[len(listValues)-nbValue:len(listValues)]
+                        norm =np.array(listValues[len(listValues)-nbValue:len(listValues)]).astype('float32')
+                        simul=preprocessing.scale(norm).reshape((1,nbValue))
+                        predicte=model.predict( np.asarray(simul).astype('float32') , batch_size=32, verbose=1)
+                        spamwriter.writerow([refidref]+predicte[0].tolist())                        
                 refidref=row.idref               
         listValues.append(row.fermeture)
                 
--- deep.py
+++ deep.py
@@ -11,15 +11,15 @@ import pandas as pd
 import numpy as np
 
 loader = pd.read_csv('alignedSorted.csv',delimiter=";")
-
+nbValue=30
 msk = np.random.rand(len(loader.index)) < 0.8
 print len(msk)
 
 
-labels = loader.ix[:,12].values.astype('int32')
+labels = loader.ix[:,nbValue+2].values.astype('int32')
 
-X_train = (loader.ix[:,1:11].values).astype('float32')[msk]
-X_test = (loader.ix[:,1:11].values).astype('float32')[~msk]
+X_train = (loader.ix[:,1:nbValue+1].values).astype('float32')[msk]
+X_test = (loader.ix[:,1:nbValue+1].values).astype('float32')[~msk]
 # convert list of labels to binary class matrix
 y_train = np_utils.to_categorical(labels)[msk]
 y_test = np_utils.to_categorical(labels)[~msk]
@@ -31,12 +31,16 @@ model = Sequential()
 # Dense(64) is a fully-connected layer with 64 hidden units.
 # in the first layer, you must specify the expected input data shape:
 # here, 20-dimensional vectors.
-model.add(Dense(640, input_dim=input_dim, init='uniform'))
+model.add(Dense(500, input_dim=input_dim, init='uniform'))
+model.add(Activation('tanh'))
+model.add(Dropout(0.6))
+model.add(Dense(250, init='uniform'))
 model.add(Activation('tanh'))
 model.add(Dropout(0.5))
-model.add(Dense(640, init='uniform'))
+model.add(Dense(50, init='uniform'))
 model.add(Activation('tanh'))
 model.add(Dropout(0.5))
+
 model.add(Dense(nb_classes, init='uniform'))
 model.add(Activation('softmax'))
 
@@ -45,8 +49,10 @@ model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 print("Training...")
-model.fit(X_train, y_train, nb_epoch=100, batch_size=100, validation_split=0.1, show_accuracy=True, verbose=2)
+model.fit(X_train, y_train, nb_epoch=100, batch_size=100, validation_split=0.1, verbose=2)
 
-score = model.evaluate(X_test, y_test, batch_size=100)
+score = model.evaluate(X_test, y_test, batch_size=10000)
 print score
 print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+model.save("model.keras")
+model.save_weights("weights.h5py")
